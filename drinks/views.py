560495baf2b857de6de6cb1fb222f4dee4a22a42
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-22 11:51:05 +0100

    app setup with db done

---
+++ drinks/views.py
@@ -0,0 +1,17 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+def drink_list(request):
+
+    #get all the drinks
+
+    drinks = Drink.objects.all()
+
+    #serialize them
+
+    serializer = DrinkSerializer(drinks, many=True)
+    
+    #return json
+
+    return JsonResponse({'drinks': serializer.data}, safe=False)
